e6990b057eca47286fecb46b688bac284f567bc3
Sonali Kisan Shinde <sonali.shinde0910@gmail.com>
2026-06-07 16:24:11 +0530

    Delete 2026/day-73/ansible-docker-project/sample directory

--- 2026/day-73/ansible-docker-project/sample/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://user:password@db:5432/jiohotstar")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
